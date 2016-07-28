31e818dac383c9566e8146716c92f3c2f445e488
robinchoii <robinchoii@gmail.com>
2016-07-28 10:04:09 -0700

    add join table and attributions

--- Trippr/db/migrate/20160728011027_create_events.rb
+++ Trippr/db/migrate/20160728011027_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+      t.integer :event_id
+      t.string :title
+      t.string :description
+      t.integer :creator_id
       t.timestamps null: false
     end
   end
---
+++ Trippr/db/migrate/20160728170220_create_user_events.rb
@@ -0,0 +1,11 @@
+class CreateUserEvents < ActiveRecord::Migration
+  def change
+    create_table :user_events do |t|
+      t.integer :user_events_id
+      t.integer :user_id
+      t.integer :event_id
+
+      t.timestamps null: false
+    end
+  end
+end
