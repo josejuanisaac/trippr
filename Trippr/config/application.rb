425bf8948c61289e172677fcf28c8494e8867245
wenwei63029869 <wwen1030@gmail.com>
2016-08-03 17:32:10 -0700

    add factory-girl gem

--- Trippr/config/application.rb
+++ Trippr/config/application.rb
@@ -12,6 +12,12 @@ module Trippr
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Skip specs file when controller generator is run
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
