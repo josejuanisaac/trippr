4e021461de1e4d30bd497c748e415f9e53ca32f4
Robin Choi <robinchoii@gmail.com>
2016-09-12 17:54:23 -0700

    server side comment lenth 140 validation

--- Trippr/app/models/comment.rb
+++ Trippr/app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :content, :post_id, :user_id, :thumbs,  presence: true
+  validates :content, length: { maximum: 140}
   belongs_to :post
   belongs_to :user
 end
